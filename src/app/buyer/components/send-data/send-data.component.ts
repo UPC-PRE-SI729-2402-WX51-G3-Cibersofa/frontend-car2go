cf24db7212291df8c788783668947c7673f3c8b5
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-09-26 11:21:28 -0500

    feat(payment): updated send-data component.

--- src/app/buyer/components/send-data/send-data.component.ts
+++ src/app/buyer/components/send-data/send-data.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-send-data',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './send-data.component.css'
 })
 export class SendDataComponent {
+  constructor(private router: Router) { }
 
+  redirectToPay() {
+    this.router.navigate(['/pay']);
+  }
 }
