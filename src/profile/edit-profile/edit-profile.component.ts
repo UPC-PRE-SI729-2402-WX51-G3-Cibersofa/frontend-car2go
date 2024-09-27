7bf6206a0ed0b1af9c699c9d7f00a3049cd9717f
✨ KARITO ✨ <108374869+Karito07@users.noreply.github.com>
2024-09-27 16:24:27 -0500

    feat(edit-profile): added @Component in edit-profile.component.ts

--- src/profile/edit-profile/edit-profile.component.ts
+++ src/profile/edit-profile/edit-profile.component.ts
@@ -3,3 +3,16 @@ import { MatCard, MatCardTitle } from '@angular/material/card';
 import { MatFormField } from '@angular/material/form-field';
 import { MatButton } from '@angular/material/button';
 import { MatInput } from '@angular/material/input';
+@Component({
+  selector: 'app-edit-profile',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatFormField,
+    MatButton,
+    MatInput
+  ],
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
