12150bd2400dde9cc19369c8ca8fa8680ad28260
✨ KARITO ✨ <108374869+Karito07@users.noreply.github.com>
2024-09-27 16:23:29 -0500

    feat(edit-profile): import MatFormField, MatButton, MatInput

--- src/profile/edit-profile/edit-profile.component.ts
+++ src/profile/edit-profile/edit-profile.component.ts
@@ -1,2 +1,5 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardTitle } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
